d5f6e430cbc4d0ada7c1537434a796af700f1f49
Kathryn Andersen <rubykat@cpan.org>
2005-05-12 11:54:54 +0000

    Import of RUBYKAT/Posy-Plugin-YamlConfig-0.42 from CPAN.
    gitpan-cpan-distribution: Posy-Plugin-YamlConfig
    gitpan-cpan-version:      0.42
    gitpan-cpan-path:         RUBYKAT/Posy-Plugin-YamlConfig-0.42.tar.gz
    gitpan-cpan-author:       RUBYKAT
    gitpan-cpan-maturity:     released

--- lib/Posy/Plugin/YamlConfig.pm
+++ lib/Posy/Plugin/YamlConfig.pm
@@ -7,11 +7,11 @@ Posy::Plugin::YamlConfig - Posy plugin to read config files in YAML format.
 
 =head1 VERSION
 
-This describes version B<0.41> of Posy::Plugin::YamlConfig.
+This describes version B<0.42> of Posy::Plugin::YamlConfig.
 
 =cut
 
-our $VERSION = '0.41';
+our $VERSION = '0.42';
 
 =head1 SYNOPSIS
 
